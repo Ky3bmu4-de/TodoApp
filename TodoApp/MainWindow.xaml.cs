ff66e4552dff6bcc44c49423df063b309dd2247f
Artem Kuzmenko <a.kuzmenko@sd-software.de>
2024-05-13 08:43:33 +0200

    Add project files.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+using TodoApp.Services;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<TodoModel> _todoDataList;
+        private FileIOService _fileIOService;
+            
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _todoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+
+
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+        }
+
+        private void _todoDataList_ListChanged(object? sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)        // we need only these 3
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+
+            }
+
+            //                                             // this is complete of cases but we don't need all of these 
+            //    switch (e.ListChangedType)
+            //{
+            //    case ListChangedType.Reset: 
+            //        break;
+            //    case ListChangedType.ItemAdded:
+            //        break;
+            //    case ListChangedType.ItemDeleted:
+            //        break;
+            //    case ListChangedType.ItemMoved:
+            //        break;
+            //    case ListChangedType.ItemChanged:
+            //        break;
+            //    case ListChangedType.PropertyDescriptorAdded:
+            //        break;
+            //    case ListChangedType.PropertyDescriptorDeleted:
+            //        break ;
+            //    case ListChangedType.PropertyDescriptorChanged:
+            //        break ;
+            //    default:    
+            //        break;
+            //}
+        }
+    }
+}
---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    internal class TodoModel: INotifyPropertyChanged
+    {
+        private bool _isDone;
+        private string _text;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+		public bool isDone
+		{
+			get { return _isDone; }
+			set 
+			{
+				if (_isDone == value)         // if doesn't change than do nothing 
+                    return;
+				_isDone = value;
+				OnPropertyChanged("IsDone");   // if something changed 
+			}
+		}
+		public string Text
+		{
+			get { return _text; }
+			set 
+			{
+				if (_text == value)
+					return;
+				_text = value;
+				OnPropertyChanged("Text");
+			}
+		}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));		// check null? 
+		}
+    }
+}
